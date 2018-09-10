03edbb7dd52ce6ccf7f9bf48af2ac9bc558461a8
Bartek Igielski <igloczek@gmail.com>
2018-09-10 22:23:59 +0200

    Fully working cleaner GraphQL demo. Deps update.

--- resources/2-data/pages/catalog/_id.vue
+++ resources/2-data/pages/catalog/_id.vue
@@ -1,14 +1,35 @@
 <template>
-  <div>
-    Category id: {{ categoryId }}
-  </div>
+  <apollo-query
+    :query="query"
+    :variables="{ categoryId }"
+  >
+    <template slot-scope="{ result: { loading, error, data } }">
+      <catalog
+        v-if="data && !error"
+        :products="data.products.items"
+        :store-config="data.storeConfig"
+        :filters="data.products.filters"
+      />
+    </template>
+  </apollo-query>
+
 </template>
 
 <script>
+  import query from '~/graphql/catalog.gql'
+
+  import Catalog from 'alpaca-components/components/04-views/catalog/Catalog.vue'
+
   export default {
+    components: {
+      Catalog
+    },
     computed: {
       categoryId() {
         return this.$route.params.id
+      },
+      query() {
+        return query
       }
     }
   }
